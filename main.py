3123e8f72295edff885f2a18ab18fa653b7624e6
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-08-19 17:28:05 -0700

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-import tkinter
 
-window = tkinter.Tk()
-window.title("My First GUI")
-window.minsize(width=500, height=300)
+# #######################################################################################
+# # Keyword Arguments
+# def my_function(a, b, c):
+#     # Do this with a
+#     # Then do this with b
+#     # Finally do this with c
+# my_function(c=3, a=1, b=2) # a,b,c in any order we want as long as keyword is in front
+#
+# # Arguments with Default Values
+# def my_function(a=1, b=2, c=3):
+#     my_function()
+#     # To modify
+#     my_function(b=5)
+# #######################################################################################
 
-# Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+# Fixed arguments
+def add(n1, n2):
+    return n1 + n2
+add(n1=5, n2=3)
 
-
-
-
-
-
-
-
-# Like a 'while True:'
-window.mainloop()
+# Unlimited Arguments
+def add(*args): # '*' is required; 'args', short for arguments is a variable.
+    for n in args:
+        print(n)
+add(n1=5, n2=3)
